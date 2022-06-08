3601ad454084a2eaac7e3ee1e56ab786aa16aebf
dawamuciil <dawamuciil@gmail.com>
2022-06-08 11:23:59 +0700

    services

---
+++ lib/services/services.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Services extends StatefulWidget {
+  Services({Key? key}) : super(key: key);
+
+  @override
+  State<Services> createState() => _ServicesState();
+}
+
+class _ServicesState extends State<Services> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
