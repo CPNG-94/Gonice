a80c3245ebc61cd33cfcfef9e2f8dd2cab4e757b
Havizh As Haq <97016516+CodeAshaq@users.noreply.github.com>
2022-06-10 22:00:16 +0700

    Update register_page.dart

--- lib/page/login/register_page.dart
+++ lib/page/login/register_page.dart
@@ -1,7 +1,5 @@
+import 'package:belajar_firebase/services/authentikasi.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:gonice/data/database/authentication.dart';
-import 'package:gonice/viewmodel/constants.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -24,237 +22,216 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            "assets/bckgr.jpg",
-            fit: BoxFit.fill,
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-          ),
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 30,
-                left: 16,
-                right: 16,
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 30,
+          left: 16,
+          right: 16,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text('Create Account',
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0)),
+              const SizedBox(
+                height: 50,
               ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Pendaftaran',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 45,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    const Text(
-                      'Nama Lengkap',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
+              const Text('Nama Lengkap',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+              const SizedBox(
+                height: 10,
+              ),
 
-                    /// KOLOM NAMA LENGKAP
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 1),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextFormField(
-                        controller: _nameController,
-                        keyboardType: TextInputType.text,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Nama Lengkap tidak boleh kosong';
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Text(
-                      'Email',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-
-                    /// KOLOM EMAIL
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 1),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextFormField(
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        decoration: const InputDecoration(
-                          border: InputBorder.none,
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Email tidak boleh kosong';
-                          } else if (!value.contains('@') ||
-                              !value.contains('.')) {
-                            return 'Format Email tidak sesuai';
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                    ),
-
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Text(
-                      'Kata Sandi',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-
-                    /// KOLOM Kata Sandi
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 1),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: TextFormField(
-                        controller: _passwordController,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        obscureText: _passwordVisible,
-                        decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                            icon: Icon((_passwordVisible)
-                                ? Icons.visibility_off
-                                : Icons.visibility),
-                            onPressed: () {
-                              setState(() {
-                                _passwordVisible = !_passwordVisible;
-                              });
-                            },
-                          ),
-                          border: InputBorder.none,
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Kata Sandi tidak boleh kosong';
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-
-                    /// LOADING INDIKATOR
-                    Visibility(
-                      visible: _visible,
-                      child: SpinKitRipple(
-                        color: Color(0xfffbbb5b),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Center(
-                      child: SizedBox(
-                        width: 170,
-                        height: 45,
-                        // ignore: deprecated_member_use
-                        child: RaisedButton(
-                          onPressed: () async {
-                            /// CEK APAKAH NAMA, EMAIL, DAN PASSWORD SUDAH TERISI DENGAN FORMAT YANG BENAR
-                            if (_formKey.currentState!.validate()) {
-                              setState(() {
-                                _visible = true;
-                              });
-
-                              bool shouldNavigate =
-                                  await Authentication.registerHandler(
-                                _emailController.text,
-                                _passwordController.text,
-                              );
-
-                              if (shouldNavigate) {
-                                await Authentication.registeringUserToDatabase(
-                                  _nameController.text,
-                                  _emailController.text,
-                                  _passwordController.text,
-                                );
-
-                                setState(() {
-                                  _visible = false;
-                                  _formKey.currentState!.reset();
-                                  _showSuccessRegistration();
-                                  _emailController.clear();
-                                  _nameController.clear();
-                                  _passwordController.clear();
-                                });
-                              } else {
-                                setState(() {
-                                  _visible = false;
-                                });
-                                _showFailureRegistration();
-                              }
-                            }
-                          },
-                          color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: const Text(
-                            'Pendaftaran',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+              /// KOLOM NAMA LENGKAP
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextFormField(
+                  controller: _nameController,
+                  keyboardType: TextInputType.text,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Nama Lengkap tidak boleh kosong';
+                    } else {
+                      return null;
+                    }
+                  },
                 ),
               ),
-            ),
-          )
-        ],
+              const SizedBox(
+                height: 10,
+              ),
+              const Text('Email address',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+              const SizedBox(
+                height: 10,
+              ),
+
+              /// KOLOM EMAIL
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Email tidak boleh kosong';
+                    } else if (!value.contains('@') || !value.contains('.')) {
+                      return 'Format Email tidak sesuai';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+              const Text('Password',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+              const SizedBox(
+                height: 10,
+              ),
+
+              /// KOLOM Kata Sandi
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 1),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextFormField(
+                  controller: _passwordController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  obscureText: _passwordVisible,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      icon: Icon((_passwordVisible)
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          _passwordVisible = !_passwordVisible;
+                        });
+                      },
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Kata Sandi tidak boleh kosong';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+
+              /// LOADING INDIKATOR
+              Visibility(
+                visible: _visible,
+                child: SpinKitRipple(
+                  color: Colors.teal,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: 140,
+                height: 45,
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                  onPressed: () async {
+                    /// CEK APAKAH NAMA, EMAIL, DAN PASSWORD SUDAH TERISI DENGAN FORMAT YANG BENAR
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        _visible = true;
+                      });
+
+                      bool shouldNavigate =
+                          await Authentication.registerHandler(
+                        _emailController.text,
+                        _passwordController.text,
+                      );
+
+                      if (shouldNavigate) {
+                        await Authentication.registeringUserToDatabase(
+                          _nameController.text,
+                          _emailController.text,
+                          _passwordController.text,
+                        );
+
+                        setState(() {
+                          _visible = false;
+                          _formKey.currentState!.reset();
+                          _showSuccessRegistration();
+                          _emailController.clear();
+                          _nameController.clear();
+                          _passwordController.clear();
+                        });
+                      } else {
+                        setState(() {
+                          _visible = false;
+                        });
+                        _showFailureRegistration();
+                      }
+                    }
+                  },
+                  color: Colors.teal,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Already have an account?',
+                      style: TextStyle(color: Colors.grey)),
+                  TextButton(
+                    child: const Text('Login',
+                        style: TextStyle(color: Colors.teal)),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -270,7 +247,7 @@ class _RegisterPageState extends State<RegisterPage> {
               Radius.circular(16),
             ),
           ),
-          backgroundColor: const Color(0xfffbbb5b),
+          backgroundColor: Colors.teal,
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
@@ -332,7 +309,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                   ),
                   decoration: BoxDecoration(
-                    color: Colors.orange,
+                    color: Colors.teal,
                     borderRadius: BorderRadius.circular(32),
                     border: Border.all(color: Colors.white, width: 2),
                   ),
