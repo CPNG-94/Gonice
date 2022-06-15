ee363288aeb0cac4216794bf297ce4960ac1235a
Farij Altaf Syah <farijaltafsyah60@gmail.com>
2022-06-15 19:11:47 +0700

    Add files via upload

---
+++ lib/controllers/excercise_db_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:test_custom/services/firestore_db.dart';
+import '../models/excercise.dart';
+import '../models/excercise2.dart';
+
+class ExcerciseDBController extends GetxController {
+
+  final excercise = <Excercise>[].obs;
+  
+  @override
+  void onInit() {
+    excercise.bindStream(FirestoreDB().getAllProducts());
+    super.onInit();
+  }
+}
+
+class ExcerciseDBController2 extends GetxController {
+
+  final excercise2 = <Excercise2>[].obs;
+
+  @override
+  void onInit() {
+    excercise2.bindStream(FirestoreDBAdded().getAllProducts());
+    super.onInit();
+  }
+}
