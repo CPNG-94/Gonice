9b4acc0e1e7b3b9a8333484f771b87b4b0b51a7e
Farij Altaf Syah <farijaltafsyah60@gmail.com>
2022-06-28 11:23:25 +0700

    Add files via upload

--- lib/models/excercise2.dart
+++ lib/models/excercise2.dart
@@ -1,8 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Excercise2 {
-
+class Saved{
   final String id;
+  final String uid;
   final String name;
   final String desc;
   final DateTime date;
@@ -10,18 +10,25 @@ class Excercise2 {
   final String vidUrl;
   final String imgUrl;
 
-  const Excercise2({required this.name, required this.desc, required this.id, required this.date, required this.rep, required this.vidUrl, required this.imgUrl});
+  Saved({
+    required this.name,
+    required this.desc,
+    required this.id,
+    required this.uid,
+    required this.date,
+    required this.rep,
+    required this.vidUrl,
+    required this.imgUrl
+  });
 
-  static Excercise2 fromSnapshot(DocumentSnapshot snap) {
-    Excercise2 excercise2 = Excercise2(
-      id: snap['id'],
-      name: snap['name'],
-      desc: snap['desc'],
-      date: (snap['date'] as Timestamp).toDate(),
-      rep: (snap['rep']),
-      vidUrl: (snap['vidUrl']),
-      imgUrl: (snap['imgUrl']),
-    );
-    return excercise2;
-  }
+  static Saved fromJson(Map<String, dynamic> json) => Saved(
+    id: json['id'],
+    uid: json['uid'],
+    name: json['name'],
+    desc: json['desc'],
+    date: (json['date'] as Timestamp).toDate(),
+    rep: (json['rep']),
+    vidUrl: (json['vidUrl']),
+    imgUrl: (json['imgUrl']),
+  );
 }
