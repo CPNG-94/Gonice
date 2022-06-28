d0a92ec7dbfc8df71a8c06cd0bb97acce2befa4f
Farij Altaf Syah <farijaltafsyah60@gmail.com>
2022-06-28 11:22:48 +0700

    Add files via upload

--- lib/services/firestore_db.dart
+++ lib/services/firestore_db.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gonice/models/excercise.dart';
-import 'package:gonice/models/excercise2.dart';
+
 
 class FirestoreDB {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
@@ -11,20 +11,6 @@ class FirestoreDB {
         .snapshots()
         .map((snapshot) {
       return snapshot.docs.map((doc) => Excercise.fromSnapshot(doc)).toList();
-    });
-  }
-}
-
-class FirestoreDBAdded {
-  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
-
-  Stream<List<Excercise2>> getAllProducts() {
-    return _firebaseFirestore
-        .collection('saved')
-        .orderBy('date', descending: false)
-        .snapshots()
-        .map((snapshot) {
-      return snapshot.docs.map((doc) => Excercise2.fromSnapshot(doc)).toList();
     });
   }
 }
